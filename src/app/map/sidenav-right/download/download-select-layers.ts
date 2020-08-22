f148b4c6c2ecc93e633d2bc0be593830be9da7b2
Karl Tayou <karltayou@Karls-MacBook-Pro.local>
2020-08-22 14:06:30 +0200

    encapsulate select layers feature in a separate class

---
+++ src/app/map/sidenav-right/download/download-select-layers.ts
@@ -0,0 +1,213 @@
+import { Component, OnInit } from '@angular/core';
+import { coucheInterface, carteInterface } from '../../../type/type';
+import { FormGroup, FormArray, FormBuilder, FormControl, AbstractControl } from '@angular/forms';
+import { StorageServiceService } from '../../../services/storage-service/storage-service.service'
+import { startWith, map, filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+/**
+ * Interface of the model that manage download features
+ */
+export interface downloadModelInterface {
+  /**
+   * Layers choose for download
+   */
+  layers: Array<coucheInterface>,
+  // /**
+  //  * Type of the region of interest it can be :
+  //  * - 'all' : download all data
+  //  * - 'draw': download data in the polygon draw by the user
+  //  * - name of the table where to find the polygon of the region of interest
+  //  */
+  // // roiType: string,
+  /**
+   * OL geometry of the region of interest
+   */
+  roiGeometry: any
+}
+/**
+ * interface of the model use to search layer to be downloded:
+ * Model that will appear in the autocomplete seach bar for download data
+ */
+export interface searchLayerToDownlodModelInterface {
+  name: string
+  description: string
+  id: number,
+  source: 'geosmCatalogue' | 'other'
+}
+
+/**
+ * For select layers user want to download
+ */
+export class selectLayersForDownload {
+  /**
+ * model that manage download features
+ */
+  downloadModel: downloadModelInterface = {} as downloadModelInterface
+
+  /**
+   * forms use to choose layers in UI
+   */
+  formsLayers: FormGroup;
+  formsLayersArray: FormArray;
+  /**
+   * list of layers that have been filterd
+   * use for autocomplete
+   */
+  filteredLayersOptions: Array<Observable<searchLayerToDownlodModelInterface[]>> = [];
+
+  /**
+   * List of all downlodable layers
+   */
+  layersDownlodable: Array<searchLayerToDownlodModelInterface> = []
+
+  constructor(
+    public StorageServiceService: StorageServiceService,
+    public fb: FormBuilder
+  ) { }
+
+
+  /**
+   * Initialise formsLayers : forms use to choose layers in UI
+   * @param boolean loadTOCLayers should we load all layers of the TOC that have the capabilities to be download ?
+   */
+  initialiseFormsLayers(loadTOCLayers: boolean = false) {
+    this.formsLayers = this.fb.group({
+      layers: this.fb.array([this.createInputFormsLayer()])
+    })
+  }
+
+  /**
+   * remove an input from the ui to choose a new layer
+   * @param i number index of the input
+   */
+  removeInputInFormsLayer(i: number) {
+    this.formsLayersArray = this.formsLayers.get('layers') as FormArray;
+    this.formsLayersArray.removeAt(i)
+    this.filteredLayersOptions.splice(i,1)
+  }
+  /**
+   * add a input in the ui to choose a new layer
+   */
+  addInputInFormsLayer() {
+    this.formsLayersArray = this.formsLayers.get('layers') as FormArray;
+    this.formsLayersArray.push(this.createInputFormsLayer());
+  }
+
+  /**
+   * create a input that will be insert in the ui to choose a new layer
+   * @return FormGroup
+   */
+  createInputFormsLayer(): FormGroup {
+    /**
+     * We first enable autocomplete on the form control
+     */
+    var newForm = new FormControl('')
+    this.filteredLayersOptions.push(
+      newForm.valueChanges
+        .pipe(
+          filter(value => typeof value == 'string'),
+          startWith(''),
+          map(value => this._filter(value))
+        )
+    )
+    return this.fb.group({
+      layer: newForm
+    })
+  }
+
+  /**
+   * Filter all layers that can be downloded by thier name
+   * @param value string value enter by the user to search a layer
+   */
+  private _filter(value: string): searchLayerToDownlodModelInterface[] {
+    const filterValue = value.toLowerCase();
+    return this.layersDownlodable.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
+  /**
+   * Use to format text that will appear after an option is choose in the autocomplete use to select layers in the UI
+   * @param layer searchLayerToDownlodModelInterface
+   * @return string
+   */
+  displayAutocompletFn(layer: searchLayerToDownlodModelInterface): string {
+    // return layer?layer.name+'('+layer.description+')':''
+    return layer ? layer.name : ''
+  }
+
+  /**
+   * funtion emit when ever a layer is selected by the user in the ui
+   * @param option MatAutocompleteSelectedEvent
+   */
+  layerSelected(option: MatAutocompleteSelectedEvent) {
+    // var layersInForm:searchLayerToDownlodModelInterface = option.option.value
+
+    this.downloadModel.layers = []
+    this.formsLayersArray = this.formsLayers.get('layers') as FormArray;
+    for (let index = 0; index < this.formsLayersArray.controls.length; index++) {
+      const element = this.formsLayersArray.controls[index];
+      var layersInForm: searchLayerToDownlodModelInterface = element.get('layer').value
+      if (layersInForm.source == 'geosmCatalogue') {
+        try {
+          this.downloadModel.layers.push(
+            this.StorageServiceService.getCouche(this.StorageServiceService.getGroupThematiqueFromIdCouche(layersInForm.id).id_thematique, layersInForm.id)
+          )
+        } catch (error) {
+
+        }
+      }
+    }
+
+  }
+
+  getAllControls():Array<AbstractControl>{
+    this.formsLayersArray = this.formsLayers.get('layers') as FormArray;
+    return this.formsLayersArray.controls
+  }
+
+  /**
+   * get all layers in the application that can be download
+   * @return Array<searchLayerToDownlodModelInterface>
+   */
+  getAllLayersDownlodable(): Array<searchLayerToDownlodModelInterface> {
+    var response: searchLayerToDownlodModelInterface[] = []
+
+    for (let iThematique = 0; iThematique < this.StorageServiceService.getAllGroupThematiques().length; iThematique++) {
+      const groupThematique = this.StorageServiceService.getAllGroupThematiques()[iThematique];
+
+      if (groupThematique.sous_thematiques) {
+        for (let index = 0; index < groupThematique.sous_thematiques.length; index++) {
+          const sous_thematique = groupThematique.sous_thematiques[index];
+          for (let jndex = 0; jndex < sous_thematique.couches.length; jndex++) {
+            const couche = sous_thematique.couches[jndex];
+            if (true) {
+              response.push({
+                name: couche.nom,
+                description: groupThematique.nom + ' / ' + sous_thematique.nom,
+                id: couche.key_couche,
+                source: 'geosmCatalogue'
+              })
+            }
+
+          }
+        }
+      } else {
+        for (let jndex = 0; jndex < groupThematique.couches.length; jndex++) {
+          const couche = groupThematique.couches[jndex];
+          if (true) {
+            response.push({
+              name: couche.nom,
+              description: groupThematique.nom,
+              id: couche.key_couche,
+              source: 'geosmCatalogue'
+            })
+          }
+        }
+      }
+    }
+
+    return response
+  }
+
+
+}
