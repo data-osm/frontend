2d41115143850adb9a99925114624fa2c4a3bdfa
yannickherve <dogneyannick@yahoo.fr>
2020-11-17 12:37:11 +0100

    add position in share url

--- src/app/map/sidenav-right/table-of-contents/table-of-contents.component.ts
+++ src/app/map/sidenav-right/table-of-contents/table-of-contents.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import { Observable, fromEvent,merge as observerMerge } from 'rxjs';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Observable, fromEvent, merge as observerMerge } from 'rxjs';
 
-import {cartoHelper, layersInMap} from '../../../../helper/carto.helper'
-import {manageCompHelper} from 'src/helper/manage-comp.helper'
+import { cartoHelper, layersInMap } from '../../../../helper/carto.helper'
+import { manageCompHelper } from 'src/helper/manage-comp.helper'
 import {
-  Map
+  Map, Transform
 } from '../../../ol-module';
-import {StorageServiceService} from 'src/app/services/storage-service/storage-service.service'
-import {ShareServiceService} from 'src/app/services/share-service/share-service.service'
+import { StorageServiceService } from 'src/app/services/storage-service/storage-service.service'
+import { ShareServiceService } from 'src/app/services/share-service/share-service.service'
 import { MatSliderChange } from '@angular/material/slider';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { map, catchError, debounceTime } from 'rxjs/operators';
@@ -27,21 +27,21 @@ import { MetadataComponent } from 'src/app/modal/metadata/metadata.component';
  */
 export class TableOfContentsComponent implements OnInit {
 
-  @Input() map:Map
+  @Input() map: Map
 
-  layersInToc:Array<layersInMap> = []
-  layerChange:Observable<any> = new Observable()
+  layersInToc: Array<layersInMap> = []
+  layerChange: Observable<any> = new Observable()
 
   constructor(
-    public StorageServiceService:StorageServiceService,
+    public StorageServiceService: StorageServiceService,
     public dialog: MatDialog,
-    public ShareServiceService:ShareServiceService,
-    public manageCompHelper:manageCompHelper
+    public ShareServiceService: ShareServiceService,
+    public manageCompHelper: manageCompHelper
   ) { }
 
   ngOnInit(): void {
 
-    this.map.getLayers().on('propertychange',(ObjectEvent)=>{
+    this.map.getLayers().on('propertychange', (ObjectEvent) => {
 
       this.getAllLayersForTOC()
     })
@@ -50,45 +50,45 @@ export class TableOfContentsComponent implements OnInit {
   /**
    * Construct the array this.layersInToc array.
    */
-  getAllLayersForTOC(){
-    let cartoHelperClass =  new cartoHelper()
+  getAllLayersForTOC() {
+    let cartoHelperClass = new cartoHelper()
 
-    let reponseLayers:Array<layersInMap> = cartoHelperClass.getAllLayersInToc()
-    let allObservableOFLayers:Array<Observable<any>> = []
+    let reponseLayers: Array<layersInMap> = cartoHelperClass.getAllLayersInToc()
+    let allObservableOFLayers: Array<Observable<any>> = []
     for (let index = 0; index < reponseLayers.length; index++) {
       const layerProp = reponseLayers[index];
       if (layerProp['type_layer'] == 'geosmCatalogue') {
-        if (layerProp['properties']['type']=='couche') {
+        if (layerProp['properties']['type'] == 'couche') {
           if (this.StorageServiceService.getGroupThematiqueById(layerProp['properties']['group_id'])) {
-            layerProp.badge={
-              text:this.StorageServiceService.getGroupThematiqueById(layerProp['properties']['group_id']).nom,
-              bgColor:this.StorageServiceService.getGroupThematiqueById(layerProp['properties']['group_id']).color
+            layerProp.badge = {
+              text: this.StorageServiceService.getGroupThematiqueById(layerProp['properties']['group_id']).nom,
+              bgColor: this.StorageServiceService.getGroupThematiqueById(layerProp['properties']['group_id']).color
             }
           }
-          layerProp['data'] = this.StorageServiceService.getCouche(layerProp['properties']['group_id'],layerProp['properties']['couche_id'])
-        }else if (layerProp['properties']['type']=='carte'){
+          layerProp['data'] = this.StorageServiceService.getCouche(layerProp['properties']['group_id'], layerProp['properties']['couche_id'])
+        } else if (layerProp['properties']['type'] == 'carte') {
 
           if (this.StorageServiceService.getGroupcarteById(layerProp['properties']['group_id'])) {
-            layerProp.badge={
-              text:this.StorageServiceService.getGroupcarteById(layerProp['properties']['group_id']).nom,
-              bgColor:this.StorageServiceService.getGroupcarteById(layerProp['properties']['group_id']).color
+            layerProp.badge = {
+              text: this.StorageServiceService.getGroupcarteById(layerProp['properties']['group_id']).nom,
+              bgColor: this.StorageServiceService.getGroupcarteById(layerProp['properties']['group_id']).color
             }
 
           }
-          layerProp['data'] = this.StorageServiceService.getCarte(layerProp['properties']['group_id'],layerProp['properties']['couche_id'])
+          layerProp['data'] = this.StorageServiceService.getCarte(layerProp['properties']['group_id'], layerProp['properties']['couche_id'])
 
         }
       }
-      allObservableOFLayers.push(fromEvent( layerProp.layer,'change:visible').pipe(map((value) => value)) )
-      allObservableOFLayers.push(fromEvent(layerProp.layer,'change:zIndex') .pipe(map((value) => value)))
-      allObservableOFLayers.push(fromEvent(layerProp.layer,'change:opacity') .pipe(map((value) => value)))
+      allObservableOFLayers.push(fromEvent(layerProp.layer, 'change:visible').pipe(map((value) => value)))
+      allObservableOFLayers.push(fromEvent(layerProp.layer, 'change:zIndex').pipe(map((value) => value)))
+      allObservableOFLayers.push(fromEvent(layerProp.layer, 'change:opacity').pipe(map((value) => value)))
     }
 
-    function compare( a, b ) {
-      if ( a.zIndex < b.zIndex ){
+    function compare(a, b) {
+      if (a.zIndex < b.zIndex) {
         return 1;
       }
-      if ( a.zIndex > b.zIndex ){
+      if (a.zIndex > b.zIndex) {
         return -1;
       }
       return 0;
@@ -100,14 +100,14 @@ export class TableOfContentsComponent implements OnInit {
         ...allObservableOFLayers
       )
       this.layerChange.
-      pipe(
-        debounceTime(1000)
-      )
-      .subscribe((response)=>{
-        this.layersInToc.sort( compare );
-      })
+        pipe(
+          debounceTime(1000)
+        )
+        .subscribe((response) => {
+          this.layersInToc.sort(compare);
+        })
     }
-    this.layersInToc.sort( compare );
+    this.layersInToc.sort(compare);
 
   }
 
@@ -116,15 +116,13 @@ export class TableOfContentsComponent implements OnInit {
    @param event CdkDragDrop<string[]>
    */
   drop(event: CdkDragDrop<string[]>) {
-    // console.log(event.previousIndex, event.currentIndex)
-    let cartoHelperClass =  new cartoHelper()
+    let cartoHelperClass = new cartoHelper()
     var layer = this.layersInToc[event.previousIndex]
-    cartoHelperClass.editZindexOfLayer(layer.layer,this.layersInToc[event.currentIndex].zIndex)
+    cartoHelperClass.editZindexOfLayer(layer.layer, this.layersInToc[event.currentIndex].zIndex)
 
     moveItemInArray(this.layersInToc, event.previousIndex, event.currentIndex);
 
     this.getAllLayersForTOC()
-    console.log(this.layersInToc)
   }
 
   /**
@@ -132,8 +130,8 @@ export class TableOfContentsComponent implements OnInit {
    * @param event MatSliderChange
    * @param layer layersInMap
    */
-  setOpactiyOfLayer(event:MatSliderChange,layer:layersInMap){
-    layer.layer.setOpacity(event.value/100)
+  setOpactiyOfLayer(event: MatSliderChange, layer: layersInMap) {
+    layer.layer.setOpacity(event.value / 100)
   }
 
   /**
@@ -141,7 +139,7 @@ export class TableOfContentsComponent implements OnInit {
    * @param event MatCheckboxChange
    * @param layer layersInMap
    */
-  setVisibleOfLayer(event:MatCheckboxChange,layer:layersInMap){
+  setVisibleOfLayer(event: MatCheckboxChange, layer: layersInMap) {
     layer.layer.setVisible(event.checked)
   }
 
@@ -149,11 +147,11 @@ export class TableOfContentsComponent implements OnInit {
    * Remove layer from map
    * @param layer layersInMap
    */
-  removeLayer(layer:layersInMap){
+  removeLayer(layer: layersInMap) {
     if (layer.type_layer == 'geosmCatalogue') {
       this.removeLayerCatalogue(layer)
-    }else{
-      let cartoHelperClass =  new cartoHelper()
+    } else {
+      let cartoHelperClass = new cartoHelper()
       cartoHelperClass.removeLayerToMap(layer.layer)
     }
   }
@@ -162,16 +160,16 @@ export class TableOfContentsComponent implements OnInit {
    * Remove layer of type cataogue from map
    * @param layer layersInMap
    */
-  removeLayerCatalogue(layer:layersInMap){
-    let cartoHelperClass =  new cartoHelper()
-    if (layer['properties']['type']=='carte') {
-      var carte:carteInterface = this.StorageServiceService.getCarte(layer.properties['group_id'],layer.properties['couche_id'])
+  removeLayerCatalogue(layer: layersInMap) {
+    let cartoHelperClass = new cartoHelper()
+    if (layer['properties']['type'] == 'carte') {
+      var carte: carteInterface = this.StorageServiceService.getCarte(layer.properties['group_id'], layer.properties['couche_id'])
       if (carte) {
         carte.check = false
       }
 
-    }else if (layer['properties']['type']=='couche'){
-      var couche:coucheInterface = this.StorageServiceService.getCouche(layer.properties['group_id'],layer.properties['couche_id'])
+    } else if (layer['properties']['type'] == 'couche') {
+      var couche: coucheInterface = this.StorageServiceService.getCouche(layer.properties['group_id'], layer.properties['couche_id'])
       if (couche) {
         couche.check = false
       }
@@ -184,55 +182,68 @@ export class TableOfContentsComponent implements OnInit {
   /**
    * remove all layer of type layersInMap in map
    */
-  clearMap(){
-    let cartoHelperClass =  new cartoHelper()
+  clearMap() {
+    let cartoHelperClass = new cartoHelper()
 
-    let reponseLayers:Array<layersInMap> = cartoHelperClass.getAllLayersInToc()
+    let reponseLayers: Array<layersInMap> = cartoHelperClass.getAllLayersInToc()
     for (let index = 0; index < reponseLayers.length; index++) {
       const layer = reponseLayers[index];
-      if (layer['properties']['type']=='carte') {
-        var carte = this.StorageServiceService.getCarte(layer['properties']['group_id'],layer['properties']['couche_id'])
+      if (layer['properties']['type'] == 'carte') {
+        var carte = this.StorageServiceService.getCarte(layer['properties']['group_id'], layer['properties']['couche_id'])
         if (!carte.principal) {
           this.removeLayer(layer)
 
         }
-      }else{
+      } else {
         this.removeLayer(layer)
       }
     }
 
   }
 
-/**
- * Share a layer
- * @param layer
- */
-  shareLayer(layer:layersInMap){
-    var params =this.ShareServiceService.shareLayer(layer.properties['type'],layer.properties['couche_id'],layer.properties['group_id'])
-    var url_share = environment.url_frontend + '/map?'+ params
-    this.manageCompHelper.openSocialShare(url_share,7)
+  /**
+   * Share a layer
+   * @param layer
+   */
+  shareLayer(layer: layersInMap) {
+    var params = this.ShareServiceService.shareLayer(layer.properties['type'], layer.properties['couche_id'], layer.properties['group_id'])
+    var url_share = environment.url_frontend + '/map?' + params
+    this.manageCompHelper.openSocialShare(url_share, 7)
 
   }
 
   /**
    * Share all layers in the toc
    */
-  shareAllLayersInToc(){
+  shareAllLayersInToc() {
     var pteToGetParams = []
     for (let index = 0; index < this.layersInToc.length; index++) {
       const layer = this.layersInToc[index];
       if (layer.tocCapabilities.share) {
         pteToGetParams.push({
-          typeLayer:layer.properties['type'],
-          id_layer:layer.properties['couche_id'],
-          group_id:layer.properties['group_id']
+          typeLayer: layer.properties['type'],
+          id_layer: layer.properties['couche_id'],
+          group_id: layer.properties['group_id']
         })
       }
 
     }
+
+    //Retrieve center's coordinates
+    var center = this.map.getView().getCenter();
+    var lonlat = Transform(center, 'EPSG:3857', 'EPSG:4326');
+    var lon = lonlat[0];
+    var lat = lonlat[1];
+    var zoom = this.map.getView().getZoom()
+
+    //var coordinateSharedLink = 'lon=' + lon + ',' + 'lat=' + lat + ',' + 'z=' + zoom
+    var coordinateSharedLink = 'pos=' + lon.toFixed(4) + ',' + lat.toFixed(4) + ',' + zoom.toFixed(4)
+
     var params = this.ShareServiceService.shareLayers(pteToGetParams)
-    var url_share = environment.url_frontend + '/map?'+ params
-    this.manageCompHelper.openSocialShare(url_share,7)
+
+    var url_share = environment.url_frontend + '/map?' + params + '&' + coordinateSharedLink
+
+    this.manageCompHelper.openSocialShare(url_share, 7)
   }
 
   /**
@@ -252,32 +263,32 @@ export class TableOfContentsComponent implements OnInit {
    * open metadata
    * @param layer layersInMap
    */
-  openMetadata(layer:layersInMap){
+  openMetadata(layer: layersInMap) {
     var metadata
     var wms_type
-    if (layer['properties']['type']=='carte') {
-      var carte = this.StorageServiceService.getCarte(layer.properties['group_id'],layer.properties['couche_id'])
+    if (layer['properties']['type'] == 'carte') {
+      var carte = this.StorageServiceService.getCarte(layer.properties['group_id'], layer.properties['couche_id'])
       metadata = carte.metadata
-    }else if (layer['properties']['type']=='couche'){
-      var couche = this.StorageServiceService.getCouche(layer.properties['group_id'],layer.properties['couche_id'])
+    } else if (layer['properties']['type'] == 'couche') {
+      var couche = this.StorageServiceService.getCouche(layer.properties['group_id'], layer.properties['couche_id'])
       metadata = couche.metadata
       wms_type = couche.wms_type
     }
 
-    if (this.displayMetadataLink(metadata) || wms_type=="osm" ) {
+    if (this.displayMetadataLink(metadata) || wms_type == "osm") {
 
       const MetaData = this.dialog.open(MetadataComponent, {
         minWidth: "350px",
         // height: '80%',
-        data: { exist:true,metadata: metadata, nom: carte?carte.nom:couche.nom, url_prefix: environment.url_prefix,data:carte?carte:couche}
+        data: { exist: true, metadata: metadata, nom: carte ? carte.nom : couche.nom, url_prefix: environment.url_prefix, data: carte ? carte : couche }
       });
 
       MetaData.afterClosed().subscribe(result => {
       });
-    }else{
+    } else {
       const MetaData = this.dialog.open(MetadataComponent, {
         minWidth: "350px",
-        data: { exist:false,metadata: metadata, nom: carte?carte.nom:couche.nom, url_prefix: environment.url_prefix,data:carte?carte:couche}
+        data: { exist: false, metadata: metadata, nom: carte ? carte.nom : couche.nom, url_prefix: environment.url_prefix, data: carte ? carte : couche }
       });
     }
 
